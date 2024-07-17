57c0d5f2b0013ace13a34c7f4c3a65e5e06feb7a
blue sun <126950883+Sakine2023@users.noreply.github.com>
2024-07-17 21:08:04 +0330

    Update errorHandler.js

--- errorHandler.js
+++ errorHandler.js
@@ -1,10 +1,14 @@
-// مدیریت خطاهای جاوا اسکریپت و جلوگیری از نمایش آنها
 window.onerror = function(message, source, lineno, colno, error) {
     console.log("JavaScript error: ", message, " at ", source, ":", lineno, ":", colno);
     return true; // از نمایش پیام خطا جلوگیری می‌کند
 };
 
 window.addEventListener('error', function(event) {
+    event.preventDefault(); // از نمایش پیام خطا جلوگیری می‌کند
+});
+
+window.addEventListener('unhandledrejection', function(event) {
+    console.log("Unhandled promise rejection: ", event.reason);
     event.preventDefault(); // از نمایش پیام خطا جلوگیری می‌کند
 });
 
